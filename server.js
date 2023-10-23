326fde4703e595d31d7777fcab6707af6563a750
shivakant <shivakantyuvasoft357@gmail.com>
2023-10-23 10:22:48 +0530

    add cors in app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require("./router/post.router");
 const { default: mongoose } = require("mongoose");
+var cors = require('cors')
 require("dotenv").config();
 require("./db/db");
 
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/v1",router);
-
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`server is runnig on port ${PORT}`);
