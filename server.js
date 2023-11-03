4e482d4d25fd781807edbe4e7a489f9f7872ee16
shivakant <shivakantyuvasoft357@gmail.com>
2023-11-03 13:07:18 +0530

    cor correction

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const express = require("express");
 const router = require("./router/post.router");
 const { default: mongoose } = require("mongoose");
 var cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors({
   "origin": "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
 }));
-
-const app = express();
-
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/v1", router);
