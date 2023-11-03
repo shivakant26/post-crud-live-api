1a801de9165e133da081d79dd9d3bd45720bb9d2
shivakant <shivakantyuvasoft357@gmail.com>
2023-11-03 13:03:11 +0530

    cors error correction

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+require("dotenv").config();
+require("./db/db");
 const express = require("express");
 const router = require("./router/post.router");
 const { default: mongoose } = require("mongoose");
-var cors = require('cors')
-require("dotenv").config();
-require("./db/db");
+var cors = require("cors");
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}));
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/v1",router);
-app.use(cors());
+app.use("/v1", router);
 
 app.listen(PORT, () => {
   console.log(`server is runnig on port ${PORT}`);
